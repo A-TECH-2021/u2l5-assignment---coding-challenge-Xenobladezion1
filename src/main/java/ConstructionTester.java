474ff93944e259ecd4dfaced60e6bab5090a2e0d
hunter.2446594 <hunter.2446594@nv.ccsd.net>
2021-10-15 13:49:26 -0700

    Coded constructionTester and HowFarAway

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -5,6 +5,24 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the sales tax rate:");
+        double taxRate = scan.nextDouble();
+        System.out.println("How many boards do you need?");
+        int numBoards = scan.nextInt();
+        System.out.println("How many windows do you need?");
+        int numWindows = scan.nextInt();
+
+
+        Construction construct = new Construction(8, 11, taxRate);
+        double total = construct.lumberCost(numBoards) + construct.windowCost(numWindows);
+        System.out.println("Total:" + total);
+        double Gtotal = construct.grandTotal(total);
+        System.out.println("Grand Total:" + Gtotal);
+
+
+
+
+
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -5,6 +5,21 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the latitude of the starting location:");
+        double Slati = scan.nextDouble();
+        System.out.println("Enter the longitude of the starting location:");
+        double Slong = scan.nextDouble();
+        System.out.println("Enter the latitude of the ending location:");
+        double Elati = scan.nextDouble();
+        System.out.println("Enter the longitude of the ending location:");
+        double Elong = scan.nextDouble();
+
+        GeoLocation first = new GeoLocation(Slati,Slong);
+        GeoLocation second = new GeoLocation(Elati,Elong);
+
+        double distance = first.distanceFrom(second);
+        System.out.println("The distance is " + distance + " miles.");
+
     }
 }
